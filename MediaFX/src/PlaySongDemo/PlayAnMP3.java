a71b2e394d82aa3a6166aa0a9bc6aa4be426184a
chriscastillo1 <ccastillo1@email.arizona.edu>
2023-02-26 12:30:05 -0700

    Made small changes to PlayOneMP3. Added name and rearranged code

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -4,6 +4,7 @@ package PlaySongDemo;
  * This code will play any song assuming that file is in folder songfiles. 
  * 
  * Programmer Rick Mercer
+ * Changer: Chris Castillo
  */
 import java.io.File;
 import java.net.URI;
@@ -17,50 +18,53 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+/*
+ * Class that plays an MP3
+ */
 public class PlayAnMP3 extends Application {
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-  private int songsPlayed = 0;
+	private int songsPlayed = 0;
 
-  @Override
-  public void start(Stage stage) throws Exception {
-    BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
-    pane.setCenter( new Label(path));
-    playASong(path);
-    // Put the pane in a sized Scene and show the GUI
-    Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
-    stage.setScene(scene);
-    // Don't forget to show the running app:
-    stage.show();
-  }
+	@Override
+	public void start(Stage stage) throws Exception {
+		BorderPane pane = new BorderPane();
 
-  
-  private void playASong(String path) {
-   
-    // Need a File and URI object so the path works on all OSs
-    File file = new File(path);
-    URI uri = file.toURI();
-    System.out.println(uri);
-    // Play one mp3 and and have code run when the song ends
-    Media media = new Media(uri.toString());
-    MediaPlayer mediaPlayer = new MediaPlayer(media);
-    mediaPlayer.play();
-      
-    mediaPlayer.setOnEndOfMedia(new Waiter());
-    System.out.println("You may need to shut this App down");
- 
-    }
-  
-  private class Waiter implements Runnable {
-    @Override
-    public void run() {
-      songsPlayed++;
-      System.out.println("Song ended, play song #" + songsPlayed);
-      Platform.exit();
-    }
-  }
+		// I, Chris Castillo, changed this song file name
+		String path = "songfiles/SwingCheese.mp3";
+		playASong(path);
+		pane.setCenter(new Label(path));
+
+		// Put the pane in a sized Scene and show the GUI
+		Scene scene = new Scene(pane, 255, 85); // 255 pixels wide, 85 pixels tall
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private void playASong(String path) {
+		// Need a File and URI object so the path works on all OSs
+		File file = new File(path);
+		URI uri = file.toURI();
+		System.out.println(uri);
+		// Play one mp3 and and have code run when the song ends
+		Media media = new Media(uri.toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+
+		mediaPlayer.setOnEndOfMedia(new Waiter());
+		System.out.println("You may need to shut this App down");
+
+	}
+
+	private class Waiter implements Runnable {
+		@Override
+		public void run() {
+			songsPlayed++;
+			System.out.println("Song ended, play song #" + songsPlayed);
+			Platform.exit();
+		}
+	}
 }
